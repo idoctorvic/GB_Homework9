ca1708ceb0d570e526319ee5bfe154013fc70ce7
idoctorvic <loukianova@yahoo.com>
2022-12-10 16:16:21 +0300

    Task 64 Reversed row of numbers

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+System.Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(number, 1));
+string PrintNumbers(int start, int end)
+{
+	if (number == 1)
+	{
+		return number.ToString();
+	}
+	return(number + " " + PrintNumbers(--number, 1));
+}
